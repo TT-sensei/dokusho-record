1d6cc52b760c2f1e3e022c7ca54aa234c898f387
TT-sensei <rlrieaeoy@gmail.com>
2026-09-12 20:43:37 +0900

    ナビキャラに読書応援メッセージと登録メッセージを追加

--- js/navi.js
+++ js/navi.js
@@ -11,6 +11,17 @@
   var CHARACTERS = ['riku', 'sora', 'kai', 'saku', 'tsuki', 'nami'];
   var DEFAULT_POSE = '07-encouraging';
 
+  var ENCOURAGEMENTS = [
+    '今日はどんな本を読もう？',
+    '1ページからでも、読書は始められるよ。',
+    '気になる本を、1冊のぞいてみよう。',
+    '読んだ本が増えると、自分の世界も広がるよ。',
+    '「読んでみたい！」を大切にしよう。',
+    '少しずつで大丈夫。今日も1ページ！',
+    '次に読む本、もう決まってる？',
+    '本を開くと、まだ知らない世界に出会えるよ。'
+  ];
+
   function escapeHtml(s) {
     return String(s || '').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
   }
@@ -20,6 +31,7 @@
   }
 
   function randomCharacter() { return CHARACTERS[Math.floor(Math.random() * CHARACTERS.length)]; }
+  function randomEncouragement() { return ENCOURAGEMENTS[Math.floor(Math.random() * ENCOURAGEMENTS.length)]; }
 
   function characterHtml(options) {
     options = options || {};
@@ -32,11 +44,19 @@
     options = options || {};
     var id = options.id || randomCharacter();
     var pose = options.pose || DEFAULT_POSE;
-    return '<div class="navi-bubble">' + characterHtml({ id: id, pose: pose }) + '<div class="navi-bubble__text">' + escapeHtml(line || '') + '</div></div>';
+    return '<div class="navi-bubble" role="note">' + characterHtml({ id: id, pose: pose }) + '<div class="navi-bubble__text">' + escapeHtml(line || '') + '</div></div>';
   }
 
   function shelfHtml() {
-    return '<div class="rr-navi-corner" aria-hidden="true">' + characterHtml({}) + '</div>';
+    return bubbleHtml(randomEncouragement(), { id: randomCharacter() });
+  }
+
+  function registeredHtml(title) {
+    var safeTitle = escapeHtml(title || 'この本');
+    return '<div class="navi-register-bubble">' +
+      characterHtml({ id: randomCharacter() }) +
+      '<div class="navi-register-bubble__text">「' + safeTitle + '」を本棚に追加したよ！<br>いいね。次はどんな本を読もう？</div>' +
+    '</div>';
   }
 
   global.RR = global.RR || {};
@@ -45,8 +65,10 @@
     DEFAULT_POSE: DEFAULT_POSE,
     characterUrl: characterUrl,
     randomCharacter: randomCharacter,
+    randomEncouragement: randomEncouragement,
     characterHtml: characterHtml,
     bubbleHtml: bubbleHtml,
-    shelfHtml: shelfHtml
+    shelfHtml: shelfHtml,
+    registeredHtml: registeredHtml
   };
 })(window);
